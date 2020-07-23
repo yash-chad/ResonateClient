376b7e8ffbefec5ad7c3a6042fbea098be613fd9
Yash Chachad <chachadyash786@gmail.com>
2020-07-23 12:23:26 +0530

    Project build files added

---
+++ build/precache-manifest.403a1c1e57db0c806fc3f9a272e83f8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd0f2bfea88314a2fbafcacb1e964e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a16c689fd93ad0728508",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "56abde903426c26f998a",
+    "url": "/static/css/main.4d5fe5b6.chunk.css"
+  },
+  {
+    "revision": "a16c689fd93ad0728508",
+    "url": "/static/js/2.26fb618c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.26fb618c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56abde903426c26f998a",
+    "url": "/static/js/main.2e02eb5b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
